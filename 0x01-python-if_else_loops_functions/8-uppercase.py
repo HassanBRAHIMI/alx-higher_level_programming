f092543c06927aa670d210eef2a050eb0d9a2939
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-03-13 19:12:24 +0000

    added

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    output = ""
+    for c in str:
+        if ord(c) <= ord('z') and ord(c) >= ord('a'):
+            output += chr(ord(c) - 32)
+        else:
+            output += c
+    print(output)
