333a8603d4cdcceb71e9c4fb431ca091822635df
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-03-19 11:48:14 +0000

    added

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for rows in matrix:
+            for columns in rows:
+                if rows[-1] != columns:
+                    print("{:d}".format(columns), end=" ")
+                else:
+                    print("{:d}".format(columns), end="")
+            print()
