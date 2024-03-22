8212c619ef93b33d2386983dfe12a026892875b1
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-03-22 22:48:51 +0000

    added

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+
+
 def best_score(a_dictionary):
+    """
+    A function that returns a key with the biggest integer value.
+    """
     if a_dictionary:
+        my_list = list(a_dictionary.keys())
         score = 0
-        name = ""
-        tada = list(a_dictionary.keys())
-        for i in tada:
+        leader = ""
+        for i in my_list:
             if a_dictionary[i] > score:
                 score = a_dictionary[i]
-                name = i
-        return i
+                leader = i
+        return leader
