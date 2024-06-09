e2636c2d101efb8bac1dc7683e5f201fd02a9fce
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-09 20:34:11 +0100

    added

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,3 +13,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+    def area(self):
+        """"calculate the are of the square"""
+        return self.__size * self.__size
