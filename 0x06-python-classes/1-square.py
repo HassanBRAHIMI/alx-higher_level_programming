7c3637bc45484060dc59d0bfa00798fbe5b65a6b
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-09 20:06:50 +0100

    added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""""create a square class"""
+
+
+class Square:
+    """the class itself"""
+
+    def __init__(self, size):
+        """inisiate size"""
+        self.__size = size
