854336db108da4951fc41f47c968e46648b37000
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-09 20:27:24 +0100

    added

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""creating a simple class"""
+
+
+class Square:
+    """""square class"""
+
+    def __init__(self, size=0):
+        """"defining attrib size"""
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
