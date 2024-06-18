76d3aa7396586cd414957ceaf0fb748fb0300843
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-18 12:23:55 +0100

    added

--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""""a simple module"""
-
-
-class BaseGeometry:
-    """base geometry"""
-    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""""a simple module"""
+
+
+class BaseGeometry:
+    """base geometry"""
+    def area(self):
+        """area method"""
+        raise Exception("area() is not implemented")
