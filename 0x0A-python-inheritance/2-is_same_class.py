30da971080194ba2531acd11fa1b4bb43fc43a8f
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-18 11:52:59 +0100

    added

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""""a simple module"""
+
+
+def is_same_class(obj, a_class):
+    """"a function that checks if an object is an instance of an a_class"""
+    return type(obj) is a_class
