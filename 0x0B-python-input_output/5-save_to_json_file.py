642524bbdeb9b53262fe7c076bc743d263a0441a
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-18 16:51:43 +0100

    added

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin
+"""a simple module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """write an obj to a json file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
