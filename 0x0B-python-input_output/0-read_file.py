27fa3c4b8a9fa2e153924d3816c7be62ea144711
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-18 15:26:02 +0100

    added

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" a simple module"""
+
+
+def read_file(filename=""):
+    """a function that reads from a file and then writes to stdout"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        readed_data = f.read()
+    f.close()
+    print(readed_data)
