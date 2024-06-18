5bc6a6f62699e6cd52f6db40b3e11989aa57b02a
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-18 15:28:44 +0100

    added

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     with open(filename, 'r', encoding="utf-8") as f:
         readed_data = f.read()
     f.close()
-    print(readed_data)
+    print(readed_data, end="")
