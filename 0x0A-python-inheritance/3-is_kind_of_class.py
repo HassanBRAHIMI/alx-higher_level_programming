9858356f97f7ca5280e617d9b87b1c740c5671ad
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-18 11:57:49 +0100

    added

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""""a simple module"""
+
+
+def is_kind_of_class(obj, a_class):
+    """"checks if an object is an instance of a specified class"""
+    return isinstance(obj, a_class)
