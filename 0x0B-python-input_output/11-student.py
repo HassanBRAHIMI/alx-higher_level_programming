96ccfdc30e56dbd61d4c601ec7ca3e6cf112fa0b
Hassan Brahimi <hbrahimi@c1r4p6.1337.ma>
2024-06-23 13:10:17 +0100

    added

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    creating a student class and getting it's dictionary
+    representation with no filtering
+"""
+
+
+class Student:
+    """that's a student yaaay"""
+    def __init__(self, first_name, last_name, age):
+        """the init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returning shit , fuck this shit"""
+        if attrs is None:
+            return self.__dict__
+        emty_dic = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                emty_dic[attr] = getattr(self, attr)
+        return emty_dic
+
+    def reload_from_json(self, json):
+        """it will replace the attributes of the instance"""
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
