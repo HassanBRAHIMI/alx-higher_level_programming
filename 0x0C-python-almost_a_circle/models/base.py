2e38fb23cf0c9d97045f0e81e6325377a4c8936f
Hassan Brahimi <hbrahimi@c1r5p8.1337.ma>
2024-06-25 16:58:36 +0100

    added

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""the base module"""
+
+
+class Base:
+    """the base class"""
+
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
