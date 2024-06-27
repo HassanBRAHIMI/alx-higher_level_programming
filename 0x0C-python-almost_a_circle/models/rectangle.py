8660ff05c4ca07096ce4d8c85dafc99998774331
HassanBRAHIMI <brahimihassan054@gmail.com>
2024-06-27 13:32:16 +0100

    added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,7 +71,7 @@ class Rectangle(Base):
     def __str__(self):
         """str method"""
         return (
-            "[Rectangle] {} {}/{} - {}/{}".format(
+            "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height
                 )
             )
